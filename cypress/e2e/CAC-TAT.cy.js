1dbda7ee198291b1e44b0b3838d2bb8006b68628
Lucas Beninca <lebbeninca@gmail.com>
2024-11-05 22:04:44 -0300

    aula 04 exercicios
    selecionando elementos em campos suspensos.

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -105,6 +105,34 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.enviandoFormularioComSucesso()
     cy.get('.success').should('be.visible')
   })
+
+
+  it('seleciona um produto (YouTube) por seu texto', () => {   
+    cy.get('#product')
+    .select('YouTube')
+    .should('have.value','youtube')
+
+  })
+
+  it('seleciona um produto (Mentoria) por seu texto', () => {   
+    cy.get('#product')
+    .select('mentoria')
+    .should('have.value','mentoria')
+
+  })
+
+
+  it('seleciona um produto (Blog) por seu índice', () => {   
+    cy.get('#product')
+    .select([1])
+    .should('have.value','blog')
+
+  })
+
+
+
+  
+
 })
 })
 
