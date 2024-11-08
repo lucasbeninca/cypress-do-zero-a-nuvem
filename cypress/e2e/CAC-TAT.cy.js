e40fddfd38c5544fc12d1ead3529c2613055eeb2
Lucas Beninca <lebbeninca@gmail.com>
2024-11-08 16:21:05 -0300

    up

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -174,7 +174,6 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     .uncheck()
     .should('not.be.checked')
 
-
    })  
 
    it('console.log', () => {  
@@ -223,5 +222,33 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     })
    })  
+
+   it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {  
+    cy.contains('a', 'Política de Privacidade')
+    .should('have.attr', 'href' , 'privacy.html')
+    .should('have.attr', 'target', '_blank')
+  
+   })  
+
+   it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {  
+    cy.contains('a','Política de Privacidade')
+    .invoke('removeAttr', 'target')
+    .click()
+  
+   })  
+
+   
+   
+
+   
+
+  
+
+
+
+
+
+
+
 })
 })
